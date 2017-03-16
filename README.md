a47bc8b01680297707d2d4df94506fda5454e155
Loren <dayan805@163.com>
2017-03-16 16:01:10 +0800

    modify dialog

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # AppUpdate
 You can use it to update your App. Support Android 7.1
+#How to Use it ?
+<pre><code>
+ AppUpdateUtils.checkUpdate(this, new AppUpdateUtils.CheckUpdateListener() {
+            @Override
+            public void checkUpdate() {
+                //add your check update interface
+                //showDialog in http callback
+                UpdateDialog.ShowUpdateDialog(MainActivity.this, "update your App", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        UpdateDialog.dismissDialog(); 
+                        Toast.makeText(MainActivity.this, "confirm button", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+</code></pre>
+#####You can find
--- update/src/main/java/pers/loren/appupdate/UpdateDialog.java
+++ update/src/main/java/pers/loren/appupdate/UpdateDialog.java
@@ -2,6 +2,8 @@ package pers.loren.appupdate;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,27 +12,49 @@ import android.widget.TextView;
  */
 
 public class UpdateDialog {
-    private static AlertDialog dialog;
 
-    public static void ShowUpdateDialog(Context mContext, String content, View.OnClickListener listener) {
+    public static void ShowUpdateDialog(Context mContext, String content, final OnConfirmListener listener) {
         final AlertDialog alertDialog = new AlertDialog.Builder(mContext, R.style.ProcessDialog)
                 .setCancelable(false).create();
-        dialog = alertDialog;
+        View view = LayoutInflater.from(mContext).inflate(R.layout.update_dialog_layout, null);
         alertDialog.show();
-        alertDialog.setContentView(R.layout.update_dialog_layout);
+        alertDialog.setContentView(view);
         ((TextView) alertDialog.findViewById(R.id.content_tv)).setText(content);
-        alertDialog.findViewById(R.id.confirm_tv).setOnClickListener(listener);
+        alertDialog.findViewById(R.id.confirm_tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+                listener.onConfirm();
+            }
+        });
         alertDialog.findViewById(R.id.cancel_tv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dismissDialog();
+                alertDialog.dismiss();
             }
         });
     }
 
-    public static void dismissDialog() {
-        if (dialog != null) {
-            dialog.dismiss();
-        }
+    public static void ShowUpdateForceDialog(Context mContext, String content, final OnConfirmListener listener) {
+        final AlertDialog alertDialog = new AlertDialog.Builder(mContext, R.style.ProcessDialog)
+                .setCancelable(false).create();
+        View view = LayoutInflater.from(mContext).inflate(R.layout.update_dialog_layout, null);
+        alertDialog.show();
+        alertDialog.setContentView(view);
+        ((TextView) alertDialog.findViewById(R.id.content_tv)).setText(content);
+        alertDialog.findViewById(R.id.confirm_tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setEnabled(false);
+                v.setBackgroundColor(Color.GRAY);
+                listener.onConfirm();
+            }
+        });
+        alertDialog.findViewById(R.id.cancel_tv).setVisibility(View.GONE);
     }
+
+    public interface OnConfirmListener {
+        void onConfirm();
+    }
+
 }
